b0bfb806c8e53455bdc4961fa03089c369879800
aubreyemma02-art <aubrey.emma02@gmail.com>
2025-08-13 14:47:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 console.log('F3FG script loaded');
-const firebaseConfig = {
-  apiKey: "AIzaSyB9J4T2hNrhCmQr10plsp3cR1rKYVZVY90",
-  authDomain: "f3fg-b61f9.firebaseapp.com",
-  databaseURL: "https://f3fg-b61f9.firebaseio.com",
-  projectId: "f3fg-b61f9",
-  storageBucket: "f3fg-b61f9.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
